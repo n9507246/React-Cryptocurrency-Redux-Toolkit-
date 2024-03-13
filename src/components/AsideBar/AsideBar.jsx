359973fe1aa0e3ff97dd2f8e687e0766d36fd85b
Nikita Balashov <noWorkingEmail@mail.ru>
2024-03-13 14:59:58 +0500

    переделан основной шаблон

--- src/components/AsideBar/AsideBar.jsx
+++ src/components/AsideBar/AsideBar.jsx
@@ -3,13 +3,15 @@ import {Typography, Avatar, Menu} from 'antd'
 import {Link} from 'react-router-dom'
 import icon from '@assets/cryptocurrency.png'
 
+import classes from './style.module.scss'
+
 export default function AsideBar(){
     return(
-        <Menu theme='dark' style={{minHeight:'100vh'}} >
+        <Menu theme='dark'>
             
-            <div>
+            <div className={classes.asiseTitle}>
                 <Avatar src={icon} size='large'/>
-                <Typography.Title level={2} className='logo'>
+                <Typography.Title level={3} className={classes.logo}>
                     <Link to='/'>Cryptoverse</Link>
                 </Typography.Title>
             </div>
